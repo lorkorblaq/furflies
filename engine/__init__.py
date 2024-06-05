c648deb2bb5df6146e43d851718b7a4533882e9e
lorkorblaq <f3miolok@gmail.com>
2024-06-05 22:49:42 +0100

    update

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,15 +1,32 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-# from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv, find_dotenv
 import os
-password="518Oloko."
-uri_production = "mongodb://clinicalx:Ilupeju2024@localhost:27017"
-uri_development = "mongodb+srv://clinicalx:518Oloko.@clinicalx.aqtbwah.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(uri_development)
-try:
-    client.admin.command('ping')
-    print("You successfully connected to Clinicalx MongoDB!")
-except Exception as e:  
-    print(e)
 
-db_clinical = client.clinicalx
+# Load environment variables from .env file
+load_dotenv(find_dotenv())
+
+# Get the MongoDB credentials from environment variables
+user = os.getenv('MONGODB_USER')
+password = os.getenv('MONGODB_PASSWORD')
+
+# Ensure user and password are retrieved correctly
+if not user or not password:
+    raise ValueError("Environment variables for MongoDB credentials are not set.")
+
+# Connection URI with additional TLS options
+# uri_development = f"mongodb+srv://{user}:{password}@project-cluster.hrihk98.mongodb.net/?retryWrites=true&w=majority"
+uri_development = f"mongodb+srv://{user}:{password}@clinicalx.aqtbwah.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri_development)
+
+try:
+    # Send a ping to confirm a successful connection
+    client.admin.command('ping')
+    print("You successfully connected to Perpetual MongoDB!")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+# Access the desired database
+db_clinical = client.project_cluster
